dafeda5557d328a3152bd2903737902ce31075df
Alok Desai <alokdesai2004@gmail.com>
2024-09-03 10:40:25 +0530

    feat: add simple http downloader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::File, io::copy};
+use tempfile::Builder;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let tmp_dir = Builder::new().prefix("example").tempdir()?;
+    let target = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+    let response = reqwest::get(target).await?;
+
+    let mut dest = {
+        let fname = response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() { None } else { Some(name) })
+            .unwrap_or("tmp.bin");
+        println!("File to downlaod: {}", fname);
+        let dir = tmp_dir.path().join(fname);
+        println!("File path: {:?}", dir);
+        File::create(dir)?
+    };
+
+    let content = response.text().await?;
+    let _ = copy(&mut content.as_bytes(), &mut dest);
+
+    Ok(())
 }
